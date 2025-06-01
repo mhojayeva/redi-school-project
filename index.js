e2f6c7398fcb01675181e984fade14c84d91cd9b
maysaaaaa <hojayevamaysa@gmail.com>
2025-06-01 15:20:26 +0200

    finshed version

--- index.js
+++ index.js
@@ -1,15 +1,19 @@
-const rates = {
-     usd: 1,
-     euro: 1.15,
-     gpb: 0.74,
-     inr: 85.47,
-     gel: 2.74
-};
+let rates = {};
 
-const dollar = "dollar"
-const euro = "euro"
-const gpb = "gpb"
-let currency = dollar
+fetch("https://my-json-server.typicode.com/mhojayeva/redi-school-project/rates")
+  .then(response => {
+    if (!response.ok) {
+      throw new Error('Network response was not ok')
+    }
+
+    return response.json()
+  })
+  .then(response => {
+    rates = response
+  }) 
+  .catch(error => {
+    alert("Could not fetch currency rates. Please check your network connection.")
+  });
 
 // change dollar to euro and euro to dollar
 // user is going to input an amount
@@ -18,49 +22,44 @@ let currency = dollar
 // and perform the multiplication
 // then return the result
 
-const valueInputNode = document.querySelector("#amount");
-const currencySelectorNode = document.querySelector("#selector");
+const valueInputNode = document.querySelector("#amount")
+const currencySelectorNode = document.querySelector("#selector")
 const outputNode = document.querySelector("#result")
 const formNode = document.querySelector("form");
 
+valueInputNode.addEventListener("change", calculateResult)
 valueInputNode.addEventListener("keyup", calculateResult)
 currencySelectorNode.addEventListener("change", calculateResult)
-
 
 function calculateResult () {
     const amount = valueInputNode.value
     const currency = currencySelectorNode.value
     const resultDisplay = document.getElementById("result")
+
     console.log("currency", currency)
+    console.log("amount", amount)
 
     if (currency === "usd-eur") {
-        console.log("amount", amount)
         const result = amount * rates.euro
-        console.log(result)
-        resultDisplay.value = result
-
-    }  if (currency === "usd-gbp") {
-        console.log("amount", amount)
+    }  
+    
+    if (currency === "usd-gbp") {
         const result = amount * rates.gpb
-        console.log(result)
-        resultDisplay.value = result
-
-    } if (currency === "eur-usd") {
-        console.log("amount", amount)
+    } 
+    
+    if (currency === "eur-usd") {
         const result = amount / rates.euro
-        console.log(result)
-        resultDisplay.value = result
-
-    } if (currency === "usd-inr") {
-        console.log("amount", amount)
+    } 
+    
+    if (currency === "usd-inr") {
         const result = amount * rates.inr
-        console.log(result)
-        resultDisplay.value = result
 
-    } if (currency === "usd-gel") {
-        console.log("amount", amount)
+    } 
+    
+    if (currency === "usd-gel") {
         const result = amount * rates.gel
-        console.log(result)
-        resultDisplay.value = result
     }
+
+    resultDisplay.value = result
+    console.log(result)
 }
